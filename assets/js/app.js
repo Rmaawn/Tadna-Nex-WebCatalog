b97f94abcf860d23ff1806d954420336b893d3eb
Arman <armanbayati52@gmail.com>
2026-04-27 12:41:35 +0330

    glassmorphism

--- assets/js/app.js
+++ assets/js/app.js
@@ -197,3 +197,13 @@ document.addEventListener("DOMContentLoaded", () => {
   render();
 
 });
+
+document.addEventListener("mousemove", e => {
+  document.querySelectorAll(".card").forEach(card => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    card.style.setProperty("--x", x + "px");
+    card.style.setProperty("--y", y + "px");
+  });
+});
